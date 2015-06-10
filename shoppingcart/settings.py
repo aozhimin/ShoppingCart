21bbb14b8f0b31d5675375d0b1166c7cfd00aff0
敖志敏 <18818237826@qq.com>
2015-06-10 20:22:37 +0800

    add static

--- shoppingcart/settings.py
+++ shoppingcart/settings.py
@@ -92,4 +92,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = '/var/wwwroot/miaowu/static'
+
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
