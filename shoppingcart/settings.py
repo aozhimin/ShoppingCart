c6289af4f3fd8e782eec77696ec8182c555bfcaa
敖志敏 <18818237826@qq.com>
2015-06-11 20:49:22 +0800

    update sql username

--- shoppingcart/settings.py
+++ shoppingcart/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'miaowu',                      # Or path to database file if using sqlite3.
-        'USER': 'aozhimin',
+        'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
