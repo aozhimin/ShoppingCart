7792e92e79981099bb7e666a4c60f0eda452e4a7
敖志敏 <18818237826@qq.com>
2015-06-12 11:44:45 +0800

    add template

--- shoppingcart/settings.py
+++ shoppingcart/settings.py
@@ -75,6 +75,14 @@ DATABASES = {
     }
 }
 
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    BASE_DIR + '/templates/',
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
