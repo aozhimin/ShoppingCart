02a9ce028426d6c87ed6b839fd56344463252bb9
敖志敏 <18818237826@qq.com>
2015-06-12 12:02:31 +0800

    modify title in django administration

--- shoppingcart/settings.py
+++ shoppingcart/settings.py
@@ -8,9 +8,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -86,7 +85,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    BASE_DIR + '/templates/',
+    PROJECT_PATH + '/templates/',
 )
 
 # Internationalization
